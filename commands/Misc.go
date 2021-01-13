5ff04666d614dcd4df9ee9f2bd682c1f578e1dd8
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2021-01-12 19:26:39 -0500

    Update Misc.go

--- commands/Misc.go
+++ commands/Misc.go
@@ -3,5 +3,5 @@ package commands
 import "github.com/bwmarrin/discordgo"
 
 func (cmd *Commands) Invite(s *discordgo.Session, message *discordgo.Message, ctx *Context) {
-	s.ChannelMessageSend(message.ChannelID, "https://top.gg/bot/775890268364210196")
+	s.ChannelMessageSend(message.ChannelID, "https://discord.com/api/oauth2/authorize?client_id=775890268364210196&permissions=8&scope=bot")
 }
