7c2241e4b8bc5e11fca85612c80efe93c176f39c
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2021-03-22 11:39:30 -0400

    fix warning redundant type from array, slice, or map composite literal

--- commands/Bot.go
+++ commands/Bot.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 
 	"github.com/Not-Cyrus/GoGuardian/events"
@@ -16,12 +17,12 @@ func (cmd *Commands) BotInfo(s *discordgo.Session, m *discordgo.Message, ctx *Co
 		Title: "Bot Info",
 
 		Fields: []*discordgo.MessageEmbedField{
-			&discordgo.MessageEmbedField{Name: "Name:", Value: utils.BotUser.Username, Inline: true},
-			&discordgo.MessageEmbedField{Name: "Server Count:", Value: fmt.Sprint(events.GuildCount), Inline: true},
-			&discordgo.MessageEmbedField{Name: "User Count:", Value: fmt.Sprint(events.MemberCount), Inline: true},
-			&discordgo.MessageEmbedField{Name: "Ping:", Value: fmt.Sprintf("%s", s.HeartbeatLatency().Round(1*time.Millisecond)), Inline: true},
-			&discordgo.MessageEmbedField{Name: "discordgo Version", Value: "v0.22.0", Inline: true},
-			&discordgo.MessageEmbedField{Name: "Shard", Value: fmt.Sprint(s.ShardID), Inline: true},
+			{Name: "Name:", Value: utils.BotUser.Username, Inline: true},
+			{Name: "Server Count:", Value: fmt.Sprint(events.GuildCount), Inline: true},
+			{Name: "User Count:", Value: fmt.Sprint(events.MemberCount), Inline: true},
+			{Name: "Ping:", Value: fmt.Sprintf("%s", s.HeartbeatLatency().Round(1*time.Millisecond)), Inline: true},
+			{Name: "discordgo Version", Value: "v0.22.0", Inline: true},
+			{Name: "Shard", Value: fmt.Sprint(s.ShardID), Inline: true},
 		},
 
 		Footer:    &discordgo.MessageEmbedFooter{Text: fmt.Sprintf("Requested by: %s | made by https://github.com/Not-Cyrus", m.Author.Username)},
@@ -34,7 +35,7 @@ func (cmd *Commands) Credits(s *discordgo.Session, m *discordgo.Message, ctx *Co
 	s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
 		Title: "Credits",
 		Fields: []*discordgo.MessageEmbedField{
-			&discordgo.MessageEmbedField{Name: "Creators:", Value: "[!fishgang Cy](https://github.com/Not-Cyrus) - Bot developer"},
+			{Name: "Creators:", Value: "[!fishgang Cy](https://github.com/Not-Cyrus) - Bot developer"},
 		},
 		Footer: &discordgo.MessageEmbedFooter{Text: fmt.Sprintf("Requested by: %s | made by https://github.com/Not-Cyrus", m.Author.Username)},
 		Color:  0x36393F,
@@ -50,7 +51,7 @@ func (cmds *Commands) Fox(s *discordgo.Session, m *discordgo.Message, ctx *Conte
 		return
 	}
 
-	maxcount, _ := strconv.Atoi(strings.TrimSuffix(string(resBody),"\n"))
+	maxcount, _ := strconv.Atoi(string(resBody))
 
 	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("https://raw.githubusercontent.com/Not-Cyrus/fox-pic-repo/main/%d.jpg", rand.Intn(maxcount-0)+0))
 }
@@ -59,7 +60,7 @@ func (cmd *Commands) Invite(s *discordgo.Session, m *discordgo.Message, ctx *Con
 
 	s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
 		Fields: []*discordgo.MessageEmbedField{
-			&discordgo.MessageEmbedField{Name: "Bot Invite", Value: fmt.Sprintf("[Click Here](https://discord.com/api/oauth2/authorize?client_id=%s&permissions=8&scope=bot)", utils.BotUser.ID), Inline: true},
+			{Name: "Bot Invite", Value: fmt.Sprintf("[Click Here](https://discord.com/api/oauth2/authorize?client_id=%s&permissions=8&scope=bot)", utils.BotUser.ID), Inline: true},
 		},
 		Footer: &discordgo.MessageEmbedFooter{Text: fmt.Sprintf("Requested by: %s | made by https://github.com/Not-Cyrus", m.Author.Username)},
 		Color:  0x36393F,
